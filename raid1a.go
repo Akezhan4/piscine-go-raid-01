154060d692f752a7eea472de4307a23859f5c689
DYernar <eduisebai.1@gmail.com>
2019-10-19 15:54:34 +0600

    sdsd

--- raid1a.go
+++ raid1a.go
@@ -5,24 +5,30 @@ import (
 )
 
 func Raid1a(x, y int) {
-	for i := 0; i < y; i++ {
-		if i == 0 || i == y-1 {
-			for j := 0; j < x; j++ {
-				if (j == 0) || (j == x-1) {
-					z01.PrintRune('o')
-				} else {
-					z01.PrintRune('-')
+	if x > 0 {
+		for i := 0; i < y; i++ {
+			if i == 0 || i == y-1 {
+				for j := 0; j < x; j++ {
+					if (j == 0) || (j == x-1) {
+						z01.PrintRune('o')
+					} else {
+						z01.PrintRune('-')
+					}
 				}
-			}
-			z01.PrintRune('\n')
-		} else if i != 0 || i != y-1 {
-			for j := 0; j < x; j++ {
-				if (j == 0) || (j == x-1) {
-					z01.PrintRune('|')
-				} else {
-					z01.PrintRune(' ')
+				z01.PrintRune('\n')
+			} else if i != 0 || i != y-1 {
+				for j := 0; j < x; j++ {
+					if (j == 0) || (j == x-1) {
+						z01.PrintRune('|')
+					} else {
+						z01.PrintRune(' ')
+					}
 				}
+				z01.PrintRune('\n')
 			}
+		}
+	} else {
+		for k := 0; k < y; k++ {
 			z01.PrintRune('\n')
 		}
 	}
